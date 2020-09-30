26a58ac5ce0c7d7178b45337d2f8316177d54aa8
samuraidrak <justinistre@hotmail.com>
2020-09-30 09:28:36 -0700

    Added Roll function, Character Selection to work with Roll function, and comments

--- Testing.py
+++ Testing.py
@@ -1,5 +1,8 @@
+import random
+
 natalia = ["Natalia", 24, 28]
 luci = ["Luci", 30, 20]
+char = ["Default", 0, 0]  #selected character array
 
 """
 How to change values
@@ -9,9 +12,6 @@ natalia[1] = weaponskill
 natalia[2] = ballisticskill
 """
 
-
-
-
 #Testing Classes, even though I could just simplify it down.
 
 class Character():
@@ -20,29 +20,54 @@ class Character():
         self.weaponskill = weaponskill
         self.ballisticskill = ballisticskill
 
-    def function(self):
+    def function(self): #Will display the character you have selected and then replace the selected character array with the array of your chosen character
         print(self.name)
         characteristics = ("WS: {} , BS: {}")
         print(characteristics.format(self.weaponskill,self.ballisticskill))
+        char.clear()  #Where it replaces selected character array with the array of your chosen character
+        char.append(self.name)
+        char.append(self.weaponskill)
+        char.append(self.ballisticskill)
+
+    def wsroll(self): #Will do a basic WeaponSkill roll for the selected character
+        z = random.randrange(1, 101)
+        sl = (self.weaponskill - z)/10
+        r = ("You rolled {}.")
+        print(r.format(z))
+        if z <= self.weaponskill:
+            hit = ("You Hit by {} SLs!")
+            print(hit.format(sl))
+        else:
+            hit = ("You Miss by {} SLs!")
+            print(hit.format(sl))
+    
+        
 
 diz = Character(natalia[0], natalia[1], natalia[2])
 vent = Character(luci[0], luci[1], luci[2])
 
+
+
 def hub():
     print("""What would you like to do today?
     [C]hoose a Character
+    [R]oll
     [Q]uit""")
     
-    y = input()
+    y = input().upper()
     if y == "C":
         choose()
+    elif y =="R":
+        c = Character(char[0],char[1],char[2])
+        c.wsroll()
+        hub()
     elif y == "Q":
         print("Thank you!  Have a nice day!")
     else:
         hub()
     
-
-def choose():
+    
+def choose(): #Choose which character to play.  Will be replaced with checking the user on discord and automatically assigning the character.
     print("""Which character would you like to play?
     [1] Natalia
     [2] Luci""")
@@ -50,7 +75,7 @@ def choose():
     if x == "1":
         diz.function()
     elif x =="2":
-        vent.function()
+        vent.function()    
     hub()
 
 hub()
