410250addd991b0f50ac5028e243aba0b2862abb
samuraidrak <justinistre@hotmail.com>
2020-10-01 11:48:48 -0700

    Tried to get bot to connect to server and run a weak program.  Failed.  Added HasPerms from Leroy. Does include Role_ID and BOT_TOKEN

--- bot.py
+++ bot.py
@@ -8,8 +8,10 @@ import logging
 
 
 LOG_PATH = "C:\\Users\\Arthur\\OneDrive\\WFRPTestingbot.log"
-#BOT_TOKEN = NzYxMjc4MzU3ODcwMzQ2MzAw.X3YRuA.Ikc-mP9HKa9smVSaTEb6uFN4a-Q
+#BOT_TOKEN = "NzYxMjc4MzU3ODcwMzQ2MzAw.X3YRuA.Ikc-mP9HKa9smVSaTEb6uFN4a-Q"
 
+ROLE_ID = 761293063238582322
+#^ This is to the new Bot Testing Server.  Only people with this role can use the bot.
 PREFIX = "WF!"
 
 #Setup Logging
@@ -19,3 +21,21 @@ logging.getLogger().addHandler(logging.StreamHandler())
 client = commands.Bot(command_prefix = PREFIX)
 client.remove_command('help')
 
+def HasPerms(ctx):
+    if ctx.author.top_role >= ctx.guild.get_role(ROLE_ID):
+        return True
+    else:
+        return False
+
+@client.event
+async def on_ready():
+    logging.info(f"Logged in as: {client.user.name}")
+    msg = "On servers:"
+    for x in client.guilds:
+        msg += " " + x.name + ","
+    logging.info(msg + " Bot is ready")
+    print(f"{client.user} has connected to Discord!")
+
+
+
+client.run(BOT_TOKEN)
