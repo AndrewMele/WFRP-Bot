e2788cce912a6a602a82835954967d7a03c4c221
samuraidrak <justinistre@hotmail.com>
2020-10-07 13:47:07 -0700

    added skill and ranged

--- bot.py
+++ bot.py
@@ -13,8 +13,8 @@ BOT_TOKEN = ""
 
 #ROLE_ID = 761293063238582322
 #^ This is to the new Bot Testing Server.  Only people with this role can use the bot.
-
-PREFIX = "WF!"
+#Bot's name is RENALD
+PREFIX = "r!"
 
 #Setup Logging
 
@@ -64,6 +64,45 @@ async def clear(ctx, number):
         msgs.append(msg)
     await ctx.message.channel.delete_messages(msgs)
 
+@client.command(aliases=["sr"])
+async def skillroll(ctx):
+    try:
+        await ctx.message.delete()
+    except:
+        print("Message already deleted.")
+    rp = random.randrange(1, 101)
+    pathletics = 30
+    pcool = 40
+    pcharm = 50
+    pskill = 0
+
+    embed = discord.Embed(colour=discord.Colour.dark_purple(),title="Would you like to use Athletics, Cool, or Charm?")
+    embed.add_field(name="Athletics",value="[1]")
+    embed.add_field(name="Cool",value="[2]")
+    embed.add_field(name="Charm",value="[3]")
+    await ctx.send(embed=embed)
+    channel=ctx.message.channel
+    def check(m):
+        return (m.content == "1" or m.content == "2" or m.content == "3") and m.channel == channel
+    response = await client.wait_for('message', check=check)
+    if response.content == "1":
+        pskill = pathletics
+    elif response.content == "2":
+        pskill = pcool
+    elif response.content == "3":
+        pskill = pcharm
+    
+    psl = (pskill -rp)/10
+
+    embed = discord.Embed(colour = discord.Colour.dark_purple(),title = f"Skill Roll for {ctx.author.name}")
+    embed.add_field(name="Roll", value=f"{rp}")
+    if rp <= pskill: 
+        embed.add_field(name="You Succeeded by", value =f"{psl}")
+    elif rp >= pskill:
+        embed.add_field(name="You Failed by", value = f"{psl}")
+
+    await ctx.send(embed=embed)
+
 @client.command(aliases = ["mr"])
 async def meleeroll(ctx):
     try:
@@ -80,9 +119,9 @@ async def meleeroll(ctx):
     pskill = 0
     oskill = 0
     
-    embed = discord.Embed(colour=discord.Colour.dark_purple(),title="Would you like to use Melee(Basic) or Melee(Brawl)?")
-    embed.add_field(name="Melee(Basic)",value="[1]")
-    embed.add_field(name="Melee(Brawl)",value="[2]")
+    embed = discord.Embed(colour=discord.Colour.dark_purple(),title="Would you like to use Melee (Basic) or Melee (Brawl)?")
+    embed.add_field(name="Melee (Basic)",value="[1]")
+    embed.add_field(name="Melee (Brawl)",value="[2]")
     await ctx.send(embed=embed)
     channel = ctx.message.channel
     def check(m):
@@ -99,6 +138,59 @@ async def meleeroll(ctx):
     embed = discord.Embed(colour = discord.Colour.dark_purple(),title = f"Melee Roll for {ctx.author.name}")
     embed.add_field(name="Roll", value=f"{rp}")
     if rp <= pskill: 
+        embed.add_field(name="Success Levels", value=f"{psl}") 
+        if crittable.checkcritical(rp):
+            embed.add_field(name= "Crit", value ="You Crit!")
+            temp_crit = crittable.crit()
+            embed.add_field(name="Crit Location", value = temp_crit, inline=False)
+            if temp_crit != "":
+                embed.add_field(name = "They Suffer:", value = crittable.doescritical(temp_crit), inline=False)
+    elif rp >= pskill:
+        embed.add_field(name = "Success Levels", value=f"{psl} SLs")
+        if crittable.checkcritical(rp):
+            embed.add_field(name="Crit", value="You Crit Failed!")
+
+    if psl >= osl and crittable.checkcritical(rp) == False:
+        embed.add_field(name= "Hit Status", value = "You Hit The Opponent",inline = False)#Make sure to put opponent name in {}
+            #^^Calculate Damage from STR/10, SL, and weapon damage vs opponent's toughness and armour.^^
+    elif psl <= osl and crittable.checkcritical(rp) == False:
+        embed.add_field(name= "Hit Status", value = "The Opponent Dodged/Parried", inline = False)#Make sure to put opponent name in {}
+   
+    await ctx.send(embed=embed)
+
+@client.command(aliases = ["rr"])
+async def rangedroll(ctx):
+    try:
+        await ctx.message.delete()
+    except:
+        print("Message already deleted.")
+    rp = random.randrange(1, 101) 
+    ro = random.randrange(1, 101)
+    prangedbow = 35 #Replace Later on to read actual character sheet info
+    prangedsling = 25
+    ododge = 40
+    pskill = 0
+    oskill = 0
+    
+    embed = discord.Embed(colour=discord.Colour.dark_purple(),title="Would you like to use Ranged (Bow) or Ranged (Sling)?")
+    embed.add_field(name="Ranged (Bow)",value="[1]")
+    embed.add_field(name="Ranged (Sling)",value="[2]")
+    await ctx.send(embed=embed)
+    channel = ctx.message.channel
+    def check(m):
+        return (m.content == "1" or m.content == "2") and m.channel == channel
+    response = await client.wait_for('message', check=check)
+    if response.content == "1":
+        pskill = prangedbow
+    elif response.content == "2":
+        pskill = prangedsling
+
+    psl = (pskill - rp)/10
+    osl = (ododge - ro)/10
+
+    embed = discord.Embed(colour = discord.Colour.dark_purple(),title = f"Ranged Roll for {ctx.author.name}")
+    embed.add_field(name="Roll", value=f"{rp}")
+    if rp <= pskill: 
         embed.add_field(name="Success Levels", value=f"{psl}")
         if crittable.checkcritical(rp):
             embed.add_field(name= "Crit", value ="You Crit!")
@@ -112,8 +204,8 @@ async def meleeroll(ctx):
             embed.add_field(name="Crit", value="You Crit Failed!")
 
     if psl >= osl and crittable.checkcritical(rp) == False:
-        embed.add_field(name= "Hit Status", value = "You Hit the Opponent",inline = False)#Make sure to put opponent name in {}
-            #Calculate Damage from STR/10, SL, and weapon damage.
+        embed.add_field(name= "Hit Status", value = "You Hit The Opponent",inline = False)#Make sure to put opponent name in {}
+            #^^Calculate Damage from STR/10, SL, and weapon damage vs opponent's toughness and armour.^^
     elif psl <= osl and crittable.checkcritical(rp) == False:
         embed.add_field(name= "Hit Status", value = "The Opponent Dodged/Parried", inline = False)#Make sure to put opponent name in {}
    
@@ -124,7 +216,7 @@ async def help(ctx):
     logging.info(f"Displaying help information to {ctx.author.name}")
     embed = discord.Embed(colour = discord.Color.dark_purple())
     embed.set_author(name="Help")
-    embed.add_field(name=PREFIX+"mroll", value="Rolls your Melee(Basic) skill against an opponent.", inline=False)
+    embed.add_field(name=PREFIX+"roll or r", value="Rolls your Melee (Basic) skill against an opponent.", inline=False)
     embed.add_field(name=PREFIX+"quote[qt]", value="Retrieves or directs random quote at mentioned users.", inline=False)
     embed.add_field(name=PREFIX+"insult[ins]", value="Retrieves or directs random insult at mentioned users.", inline=False)
     embed.add_field(name=PREFIX+"search[srh]", value="Searches the list of insults.", inline=False)
